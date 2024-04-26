66b312cbcf96d16161013f90b41b3e6a33722313
joshtin2505 <justincastro2505@gmail.com>
2024-04-26 12:17:56 -0500

    Update route for getting CV by ID in cv.routes.ts

--- src/routes/cv.routes.ts
+++ src/routes/cv.routes.ts
@@ -2,6 +2,6 @@ import { Router } from 'express'
 import { getCVById } from '../controllers/cv.controllers'
 const router = Router()
 
-router.get('/get-cv', getCVById)
+router.get('/get-cv/:id/', getCVById)
 
 export default router
